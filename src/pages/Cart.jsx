a7137c1dd0f8e93f91bc109adb693512504c8e15
TeslenkoNV <natella911@mail.ru>
2024-09-16 16:16:51 +0300

    Cart's changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -44,8 +44,8 @@ const Cart = () => {
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
                     <div className='flex items-center gap-5 mt-2'>
-                      <p>{currency}{productData.price}</p>
-                      <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                      <p className='w-20'>{currency}{productData.price}</p>
+                      <p className='w-15 px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
                     </div>
                   </div>
                 </div>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className='m-10 flex-col justify-center'>
             <p className='text-5xl mb-2'>Uh oh, that page no longer exists...</p>
-            <p>We're very sorry, the page you have requested could not be found or the URL has been typed incorrectly.</p>
+            <p>We are very sorry, the page you have requested could not be found or the URL has been typed incorrectly.</p>
             <p>Here are some helpful links:</p>
             <Link className='underline underline-offset-8 text-lg' to='/'>Home</Link><br />
             <Link className='underline underline-offset-8 text-lg' to='/collection'>Collection</Link><br />
